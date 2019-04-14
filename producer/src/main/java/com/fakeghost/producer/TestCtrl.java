1c9a4d167a05bcb87c41e51094282543fa02084f
blackfur <hj8558@163.com>
2019-04-14 19:28:24 +0800

    hystrix fallback

--- producer/src/main/java/com/fakeghost/producer/TestCtrl.java
+++ producer/src/main/java/com/fakeghost/producer/TestCtrl.java
@@ -3,11 +3,13 @@ package com.fakeghost.producer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class TestCtrl{
 
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+   @HystrixCommand(fallbackMethod = "getDataFallBack")
 	public Employee firstPage() {
 
 		Employee emp = new Employee();
@@ -16,7 +18,22 @@ public class TestCtrl{
 		emp.setEmpId("1");
 		emp.setSalary(3000);
 
+      if(emp.getName().equalsIgnoreCase("emp1"))
+         throw new RuntimeException();
+
 		return emp;
+	}
+
+   public Employee getDataFallBack() {
+		
+		Employee emp = new Employee();
+		emp.setName("fallback-emp1");
+		emp.setDesignation("fallback-manager");
+		emp.setEmpId("fallback-1");
+		emp.setSalary(3000);
+
+		return emp;
+		
 	}
 
 }
