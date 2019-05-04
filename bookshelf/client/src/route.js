844cf682c7bd37182612771aa4ada62410d74946
blackfur <hj8558@163.com>
2019-05-04 21:45:14 +0800

    dynamic route

--- bookshelf/client/src/route.js
+++ bookshelf/client/src/route.js
@@ -12,11 +12,19 @@ const Login = {
 const About = {
    template: '<div>About</div>'
 }
+const Post = {
+  template: '<div>Post: {{ $route.params.post_slug }} from {{ $route.params.author }}</div>',
+  beforeRouteUpdate(to, from, next) {
+    this.$log.debug(`Updating slug from ${from} to ${to}`)
+    next() //make sure you always call next()
+  }
+}
 const router = new VueRouter({
    routes: [
       { path: '/', component: Home },
       { path: '/login', component: Login },
-      { path: '/about', component: About }
+      { path: '/about', component: About },
+      { path: '/post/:author/:post_slug', component: Post }
    ]
 })
 
