5558ad172665c3508dca4005ee65fd44e357a599
junhuang <junhuang@powerbridge.com>
2019-05-06 14:22:03 +0800

    MockMvc with full Spring application Context

--- bookshelf/src/test/java/com/fakeghost/bookshelf/ApplTest.java
+++ bookshelf/src/test/java/com/fakeghost/bookshelf/ApplTest.java
@@ -1,34 +1,31 @@
 package com.fakeghost.bookshelf;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ApplTest {
 
    @Autowired
-   TestCtrl ctrl;
-
-   @LocalServerPort
-    int port;
-
-    @Autowired
-    TestRestTemplate restTemplate;
+   MockMvc mockMvc;
 
     @Test
     public void sample() throws Exception {
-       assertThat(ctrl).isNotNull();
-       assertThat(restTemplate.getForObject(
-                "http://localhost:" + port + "/sample",
-                String.class)).contains("sample");
+      this.mockMvc.perform(get("/sample")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("sample")));
     }
 
 }
