e0bff99cfd66030a3fd949c786095e3fe9af6212
junhuang <junhuang@powerbridge.com>
2019-05-06 14:44:05 +0800

    basic junit4 test sample

---
+++ bookshelf/src/test/java/com/fakeghost/bookshelf/AppTest.java
@@ -0,0 +1,12 @@
+package com.fakeghost.bookshelf;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+   @Test
+    public void sample() {
+        assertTrue( true );
+    }
+}
