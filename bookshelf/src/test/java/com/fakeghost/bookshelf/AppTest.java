1067d4a39c94ca52309f185c17dfbc3721096e1d
junhuang <junhuang@powerbridge.com>
2019-05-07 14:20:02 +0800

    lombok Slf4j

--- bookshelf/src/test/java/com/fakeghost/bookshelf/AppTest.java
+++ bookshelf/src/test/java/com/fakeghost/bookshelf/AppTest.java
@@ -2,11 +2,14 @@ package com.fakeghost.bookshelf;
 
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AppTest {
 
    @Test
     public void sample() {
+       log.error("support by Slf4j");
         assertTrue( true );
     }
 }
