ea68e80c104927b81f0f454514c8352575e1dbd3
blackfur <hj8558@163.com>
2019-05-14 01:46:28 +0800

    fss apk init

---
+++ freess_apk/src/test/Sample.kt
@@ -0,0 +1,11 @@
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import static System.out.println
+
+class EmailValidatorTest {
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+       println("I'm new to kotlin.")
+        assertThat(true).isTrue()
+    }
+}
