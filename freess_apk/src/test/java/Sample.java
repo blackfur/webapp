10363dca7e9f6692998765a7b6efa1f8c6b5d5be
blackfur <hj8558@163.com>
2019-05-20 00:43:02 +0800

    fix: fss create process fail.

--- freess_apk/src/test/java/Sample.java
+++ freess_apk/src/test/java/Sample.java
@@ -2,10 +2,17 @@ import com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 import static System.out.println;
 
-public class EmailValidatorTest {
+public class Sample {
     @Test
     public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
        println("Sample Test.");
         assertThat(true).isTrue();
     }
+   @Test(expected = NullPointerException.class)
+   public void nullStringTest() {
+      String str = null;
+      assertTrue(str.isEmpty());
+   }
+   @Test(timeout = 1000)
+    public void request(){}
 }
