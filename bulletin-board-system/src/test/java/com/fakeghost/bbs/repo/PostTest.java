48bc0ab373e264b01f2cfb54e22ef9a8a4ef5ef0
junhuang <junhuang@powerbridge.com>
2019-05-24 13:17:47 +0800

    hibernate transaction

--- bulletin-board-system/src/test/java/com/fakeghost/bbs/repo/PostTest.java
+++ bulletin-board-system/src/test/java/com/fakeghost/bbs/repo/PostTest.java
@@ -1,8 +1,10 @@
 package com.fakeghost.bbs.repo;
 
+import org.hibernate.SessionFactory;
 import com.fakeghost.bbs.App;
 import com.fakeghost.bbs.conf.AppConf;
 import com.fakeghost.bbs.model.Post;
+import org.hibernate.criterion.Projections;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -12,17 +14,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+import javax.transaction.Transactional;
 import java.util.Iterator;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = {App.class}, webEnvironment=WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("dev")
+@Transactional
 public class PostTest {
     final Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     PostRepo repo;
+    @Autowired SessionFactory sessionf;
+
     @Test
     public void posts_ShouldReturnAllPosts(){
 		 repo.save(new Post("CN", "CN Sucks."));
@@ -31,5 +40,13 @@ public class PostTest {
         assertTrue(transvers.hasNext());
         Post one = transvers .next();
         log.info(one.toString());
+    }
+    @Test
+    public void ShouldReturnNumber(){
+        Integer all= ((Long)sessionf.getCurrentSession().createQuery("select count(1) from Post").uniqueResult()).intValue();
+        //Long all = (Long)sessionf.getCurrentSession().createCriteria("Post") .setProjection(Projections.rowCount()) .uniqueResult();
+        // Integer all = sessionf.getCurrentSession().createQuery("select count(1) from Post").getFirstResult();
+        assertTrue( null !=all && all >= 0);
+        log.info(all.toString());
     }
 }
