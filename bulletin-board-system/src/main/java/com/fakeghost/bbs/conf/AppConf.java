7d45852dd705ce71d4a6124f3c31cb2c07ff6cb4
junhuang <junhuang@powerbridge.com>
2019-05-29 14:33:33 +0800

    Swagger

--- bulletin-board-system/src/main/java/com/fakeghost/bbs/conf/AppConf.java
+++ bulletin-board-system/src/main/java/com/fakeghost/bbs/conf/AppConf.java
@@ -1,12 +1,26 @@
 package com.fakeghost.bbs.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.Environment;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ImportResource("classpath:spring.xml")
+@EnableSwagger2
 //@ImportResource(value="classpath:context.properties", reader=PropertiesBeanDefinitionReader.class)
 public class AppConf {
-
+   @Autowired
+   Environment env;
+   @Bean
+   public Docket productApi() {
+      return new Docket(DocumentationType.SWAGGER_2).select()
+         .apis(RequestHandlerSelectors.basePackage(env.getProperty("PACKAGE"))).build();
+   }
 }
 
