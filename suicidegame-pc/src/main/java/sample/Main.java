b3a47a21b2dcd88b94ed3d8c407cb805a3ff6455
junhuang <junhuang@powerbridge.com>
2019-06-17 10:51:59 +0800

    log4j

--- suicidegame-pc/src/main/java/sample/Main.java
+++ suicidegame-pc/src/main/java/sample/Main.java
@@ -7,20 +7,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class Main extends Application {
+    Log log = LogFactory.getLog(getClass());
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         URL resource = getClass().getResource("/sample.fxml");
         if(null == resource){
-            throw new Exception("file Not found.");
+            String errmsg = "file Not found.";
+            log.error(errmsg);
+            throw new Exception(errmsg);
         }
         Parent root = FXMLLoader.load(resource);
         primaryStage.setTitle("Welcome to Heaven.");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        log.info("load Success.");
     }
 
 
