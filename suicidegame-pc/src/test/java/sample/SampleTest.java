09da36938f0e5559b6bf9aa3dcc918d541b5e4a4
junhuang <junhuang@powerbridge.com>
2019-06-17 14:29:13 +0800

    test Javafx app

--- suicidegame-pc/src/test/java/sample/SampleTest.java
+++ suicidegame-pc/src/test/java/sample/SampleTest.java
@@ -1,10 +1,22 @@
 package sample;
 
+import javafx.application.Application;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class SampleTest {
+    @BeforeClass
+    public static void init(){
+        Thread thread = new Thread(){
+            public void run(){
+                Application.launch(Main.class);
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+    }
     @Test
     public void should_Success(){
         assertEquals(0, 0);
