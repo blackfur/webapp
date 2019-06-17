d4f1474177bda35d471e3c2ee1b37f233d016503
junhuang <junhuang@powerbridge.com>
2019-06-17 14:25:29 +0800

    unit test 4

---
+++ suicidegame-pc/src/test/java/sample/SampleTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void should_Success(){
+        assertEquals(0, 0);
+    }
+}
