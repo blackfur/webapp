3dc70bde073c0d796e21e504bd7541da490394be
junhuang <junhuang@powerbridge.com>
2019-06-17 17:41:25 +0800

    launch script

--- suicidegame-pc/build.bat
+++ suicidegame-pc/build.bat
@@ -2,7 +2,7 @@
 
 @set Name=suicidegamepc
 @set Version=1.0-SNAPSHOT
-@set bin=target\%Name%-%Version%.jar
+@set bin=target\%Name%-%Version%-jar-with-dependencies.jar
 
 @if "%1" == "launch" goto launch
 @goto :EOF
